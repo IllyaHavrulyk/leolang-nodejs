951a2c6c062297f48b017ce0deb824fbe950e070
Sviatoslav25 <sviatoslavvintoniv@gmail.com>
2021-07-19 00:09:17 +0300

    setup eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,18 @@
 module.exports = {
-    env: {
-      browser: true,
-      es2021: true,
-    },
-    extends: ['airbnb-base', 'plugin:prettier/recommended'],
-    parserOptions: {
-      ecmaVersion: 12,
-      sourceType: 'module',
-    },
-    rules: {},
-  };
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['airbnb-base', 'plugin:prettier/recommended'],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  rules: {
+    'prettier/prettier': 0,
+    'no-use-before-define': 0,
+    'no-underscore-dangle': 0,
+    'no-unused-vars': 1,
+    'class-methods-use-this': 0,
+  },
+};
